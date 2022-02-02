6b8ac2aab9e7967642170b142d1164481ab3c6c3
rohit <mrsbeproject@gmail.com>
2022-02-02 18:53:36 +0530

    rohit v0

---
+++ app/src/main/java/com/mymusic/repository/RestRepository.java
@@ -0,0 +1,10 @@
+package com.mymusic.repository;
+
+import java.util.List;
+
+public class RestRepository {
+    List<String> recommend(List<String> songs){
+        List<String> rec = new List();
+
+    }
+}
